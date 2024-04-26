1382dc45b00bb9fa4dc5bc4bcdd9942940d008e8
hasanullahkhan23 <hasanukpk@gmail.com>
2024-04-26 21:06:48 +0500

    push my todo list to github

---
+++ main.ts
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let todos = [];
+
+let condision = true;
+
+while (condision){
+
+let todoQuestion = await inquirer.prompt(
+    [
+        {
+            name: "firstQuestion",
+            message: "Do you want to add in your Todos?",
+            type: "input"
+        },
+
+        {
+            name:"secondQuestion",
+            message: "Do you want add more in your Todos?",
+            type: "confirm",
+            default: "true"
+        }
+    ]
+);
+
+todos.push(todoQuestion.firstQuestion);
+condision = todoQuestion.secondQuestion
+console.log(todos);
+}
